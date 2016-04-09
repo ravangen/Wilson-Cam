e3fb941a49719c07df733e1948ba6a34af0956fb
Rob Van Gennip <rvangennip@waveaccounting.com>
2016-04-09 19:09:19 -0400

    Use full file paths

--- camera.py
+++ camera.py
@@ -1,18 +1,22 @@
-from os import getenv
+from os import getenv, path
 from subprocess import call
 
 import dropbox
 
 
 DROPBOX_IMAGE_NAME = '/img.jpeg'
+FSWEBCAM_CONFIG_NAME = 'fswebcam.config'
 
 
-def generate():
-    call('fswebcam --config fswebcam.config image.jpeg', shell=True)
-    return 'image.jpeg'
+def generate(file_name='image.jpeg'):
+    directory = path.dirname(path.realpath(__file__))
+    config_path = path.join(directory, FSWEBCAM_CONFIG_NAME)
+    image_path = path.join(directory, file_name)
+    call('fswebcam --config {config} {image}'.format(config=config_path, image=image_path), shell=True)
+    return image_path
 
 
-def upload(file_name):
+def upload(file_path):
     access_token = getenv('DROPBOX_TOKEN')
     if not access_token:
         raise ValueError('DROPBOX_TOKEN not set')
@@ -26,7 +30,7 @@ def upload(file_name):
     parent_rev = file_metadata.get('rev')
 
     # Upload new version of image
-    file_obj = open(file_name, 'rb')
+    file_obj = open(file_path, 'rb')
     try:
         response = client.put_file(full_path=DROPBOX_IMAGE_NAME, file_obj=file_obj, parent_rev=parent_rev)
         print 'uploaded: ', response
@@ -37,8 +41,8 @@ def upload(file_name):
 
 
 def main():
-    file_name = generate()
-    upload(file_name)
+    file_path = generate()
+    upload(file_path)
 
 
 if __name__ == '__main__':
